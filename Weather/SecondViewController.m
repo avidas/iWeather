339c63d5ad39719c1cbec70316bb1763d0d0dcd6
Avi Das <adas@colgate.edu>
2014-07-29 23:43:19 -0500

    Add tableview in second view under tab controller to show search history from core data

--- Weather/SecondViewController.m
+++ Weather/SecondViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "SecondViewController.h"
+#import "AppDelegate.h"
 
 @interface SecondViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *name;
+@property (strong, nonatomic) NSMutableArray *temperature;
 
 @end
 
@@ -18,6 +22,52 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.name = [[NSMutableArray alloc] init];
+    self.temperature = [[NSMutableArray alloc] init];
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    NSEntityDescription *entityDesc = [NSEntityDescription entityForName:@"CityTemperature" inManagedObjectContext:context];
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entityDesc];
+    NSManagedObject *matches = nil;
+    
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request error:&error];
+    if ([objects count] == 0)
+    {
+        NSLog(@"No matching records found.");
+    } else {
+        for (int i=0; i < objects.count; i++)
+        {
+            matches = objects[i];
+            [self.name addObject:[matches valueForKey:@"name"]];
+            [self.temperature addObject:[matches valueForKey:@"temperature"]];
+        }
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.name.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellID = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellID];
+    }
+    cell.textLabel.text = [self.name objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [self.temperature objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning
