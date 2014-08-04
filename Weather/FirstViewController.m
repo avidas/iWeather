9a7bb2deeb76a0f011870ef1c962a589a4fafab7
Avi Das <adas@colgate.edu>
2014-08-04 07:01:31 -0500

    Commenting and remove model class

--- Weather/FirstViewController.m
+++ Weather/FirstViewController.m
@@ -1,6 +1,7 @@
 //
 //  FirstViewController.m
 //  Weather
+//  Search location, display on mapview with weather from forecast.io and save in core data
 //
 //  Created by Das, Ananya on 7/26/14.
 //  Copyright (c) 2014 Das, Ananya. All rights reserved.
--- Weather/WeatherAnnotation.h
+++ Weather/WeatherAnnotation.h
@@ -11,7 +11,6 @@
 
 @interface WeatherAnnotation : NSObject <MKAnnotation>
 
-//why copy?
 //these must have names title and subtitle
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *subtitle;
